43a5f5c77cb169781fdab994e5e0e7d73d191053
Piyush Bhatee <piyushbhatee@Piyushs-MacBook-Pro.local>
2024-07-10 15:43:10 +0530

    huehue

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // Define routes
 app.get('/', (req, res) => {
-  res.send('Hello World Frm Bhatee!');
+  res.send('Hello World From Bhatee!');
 });
 
 app.get('/env', (req, res) => {
