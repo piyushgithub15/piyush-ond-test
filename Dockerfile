c35b76e4573ee04ce3e8795549643319296dd9cd
Piyush Bhatee <piyushbhatee@Piyushs-MacBook-Pro.local>
2024-07-10 15:08:57 +0530

    huehue

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:22-alpine3.19
+WORKDIR /src
+COPY package*.json ./
+RUN npm install
+COPY . .
+CMD ["npm", "start"]
+EXPOSE 3000 3001
---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Define routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/env', (req, res) => {
+  const envs = {
+    port: process.env.PORT,
+    dbHost: process.env.DB_HOST,
+    dbUser: process.env.DB_USER,
+    dbPass: process.env.DB_PASS,
+  };
+  res.json(envs);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
